fb3e1245f692dc18847c55358ebdff8af7c86a67
Carl Johan Schedvin <cj.schedvin@gmail.com>
2022-12-04 09:31:21 +0100

    AoC Part-2 December 4th 2022

---
+++ 2022/4/part2/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"advent/helpers"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+
+type SectionAssignment struct {
+  start int
+  end int
+}
+
+
+func NewSectionAssignment(value []string)(SectionAssignment) {
+
+  start, _ := strconv.Atoi(value[0])
+  end, _ := strconv.Atoi(value[1])
+
+  return SectionAssignment { 
+    start, 
+    end,
+  }
+}
+
+func ParseSectionAssignments(value string)(SectionAssignment, SectionAssignment) {
+
+  pair := strings.Split(value, ",")
+
+  section1 := strings.Split(pair[0], "-")
+  section2 := strings.Split(pair[1], "-")
+
+  return NewSectionAssignment(section1), NewSectionAssignment(section2)
+}
+
+
+ func InBetween(i, min, max int) bool {
+  if (i >= min) && (i <= max) {
+    return true
+  } else {
+    return false
+  }
+ }
+
+func main() {
+
+  lines, _ := helpers.ReadLines("../puzzle-input")
+  count := 0
+
+  for _, line := range lines {
+
+    first, second := ParseSectionAssignments(line)
+    
+      
+    if InBetween(first.start, second.start, second.end) ||
+        InBetween(first.end, second.start, second.end) {
+      count++
+      continue
+    }
+
+    if InBetween(second.start, first.start, first.end) ||
+        InBetween(second.end, first.start, first.end) {
+      count++
+    }
+  }
+
+  fmt.Println(count)
+}
