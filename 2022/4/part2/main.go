8665a07d1ad9a2e6ba55df5973b010f9b1dc0de3
Carl Johan Schedvin <cj.schedvin@gmail.com>
2022-12-04 09:45:31 +0100

    Added null-ls/gofmt formatting in neovim

--- 2022/4/part2/main.go
+++ 2022/4/part2/main.go
@@ -7,64 +7,59 @@ import (
 	"strings"
 )
 
-
 type SectionAssignment struct {
-  start int
-  end int
+	start int
+	end   int
 }
 
+func NewSectionAssignment(value []string) SectionAssignment {
 
-func NewSectionAssignment(value []string)(SectionAssignment) {
+	start, _ := strconv.Atoi(value[0])
+	end, _ := strconv.Atoi(value[1])
 
-  start, _ := strconv.Atoi(value[0])
-  end, _ := strconv.Atoi(value[1])
-
-  return SectionAssignment { 
-    start, 
-    end,
-  }
+	return SectionAssignment{
+		start,
+		end,
+	}
 }
 
-func ParseSectionAssignments(value string)(SectionAssignment, SectionAssignment) {
+func ParseSectionAssignments(value string) (SectionAssignment, SectionAssignment) {
 
-  pair := strings.Split(value, ",")
+	pair := strings.Split(value, ",")
 
-  section1 := strings.Split(pair[0], "-")
-  section2 := strings.Split(pair[1], "-")
+	section1 := strings.Split(pair[0], "-")
+	section2 := strings.Split(pair[1], "-")
 
-  return NewSectionAssignment(section1), NewSectionAssignment(section2)
+	return NewSectionAssignment(section1), NewSectionAssignment(section2)
 }
 
-
- func InBetween(i, min, max int) bool {
-  if (i >= min) && (i <= max) {
-    return true
-  } else {
-    return false
-  }
- }
+func InBetween(i, min, max int) bool {
+	if (i >= min) && (i <= max) {
+		return true
+	} else {
+		return false
+	}
+}
 
 func main() {
 
-  lines, _ := helpers.ReadLines("../puzzle-input")
-  count := 0
+	lines, _ := helpers.ReadLines("../puzzle-input")
+	count := 0
 
-  for _, line := range lines {
+	for _, line := range lines {
+		first, second := ParseSectionAssignments(line)
 
-    first, second := ParseSectionAssignments(line)
-    
-      
-    if InBetween(first.start, second.start, second.end) ||
-        InBetween(first.end, second.start, second.end) {
-      count++
-      continue
-    }
+		if InBetween(first.start, second.start, second.end) ||
+			InBetween(first.end, second.start, second.end) {
+			count++
+			continue
+		}
 
-    if InBetween(second.start, first.start, first.end) ||
-        InBetween(second.end, first.start, first.end) {
-      count++
-    }
-  }
+		if InBetween(second.start, first.start, first.end) ||
+			InBetween(second.end, first.start, first.end) {
+			count++
+		}
+	}
 
-  fmt.Println(count)
+	fmt.Println(count)
 }
